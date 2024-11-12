f8466c14eb023adb1848307ebaa6efcc7e785a29
Leocchi <128828635+Leolouise26@users.noreply.github.com>
2024-11-12 22:42:27 +0800

    logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,7 +21,7 @@ export const metadata = {
   openGraph: {
     images: [
       {
-        url: "/logo-new.jpg",
+        url: "/leocchi-new-logo.jpg",
         width: 500, // Update as needed
         height: 800, // Update as needed
         alt: "Logo for Portfolio",
