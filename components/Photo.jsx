d9ced17234a928cf88579c56f1857cbbc238113d
Leocchi <128828635+Leolouise26@users.noreply.github.com>
2024-11-12 17:40:33 +0800

    photo

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -17,7 +17,7 @@ const Photo =() => {
                 }}
                 className="w-[298px] h-[298px] xl:w-[498px]
                 xl:h-[498px] mix-blend-lighten absolute">
-                    <Image src="/assets/Photo.png" priority quality={100} fill 
+                    <Image src="/assets/Photo.webp" priority quality={100} fill 
                     alt="" className="object-contain" />
                 </motion.div>
 
