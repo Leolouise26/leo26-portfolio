ffb879ec85ad5d0fd0582172e8c02e312456657a
Leocchi <128828635+Leolouise26@users.noreply.github.com>
2024-11-12 20:04:01 +0800

    aria-label

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -5,15 +5,15 @@ import {RiInstagramLine, RiFacebookLine, RiTwitterLine, RiTiktokLine, RiGithubLi
 const Socials = () => {
     return(
         <div className="flex items-center gap-x-5 text-lg">
-            <Link href={'https://www.facebook.com/hiddenbladesueeeee/'} target="_blank" className="hover:text-accent transition all duration-300">
+            <Link href={'https://www.facebook.com/hiddenbladesueeeee/'} aria-label="Facebook" target="_blank" className="hover:text-accent transition all duration-300">
             <RiFacebookLine /></Link>
-            <Link href={'https://www.instagram.com/leeeeeeeyoooww/'} target="_blank" className="hover:text-accent transition all duration-300">
+            <Link href={'https://www.instagram.com/leeeeeeeyoooww/'} aria-label="Instagram" target="_blank" className="hover:text-accent transition all duration-300">
             <RiInstagramLine /></Link>
-            <Link href={'https://x.com/leolouise261'} target="_blank" className="hover:text-accent transition all duration-300">
+            <Link href={'https://x.com/leolouise261'} aria-label="Twitter" target="_blank" className="hover:text-accent transition all duration-300">
             <RiTwitterLine /></Link>
-            <Link href={'https://www.tiktok.com/@leocchi26?lang=en'} target="_blank" className="hover:text-accent transition all duration-300">
+            <Link href={'https://www.tiktok.com/@leocchi26?lang=en'} aria-label="Tiktok" target="_blank" className="hover:text-accent transition all duration-300">
             <RiTiktokLine /></Link>
-            <Link href={'https://github.com/Leolouise26'} target="_blank" className="hover:text-accent transition all duration-300">
+            <Link href={'https://github.com/Leolouise26'} aria-label="GitHub" target="_blank" className="hover:text-accent transition all duration-300">
             <RiGithubLine /></Link>
         </div>
     )
